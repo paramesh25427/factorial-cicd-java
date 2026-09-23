a7cc5e80cd458f7cf373a710df8956cccb88cc39
paramesh25427 <paramesh25427@gmail.com>
2026-09-23 09:36:42 +0530

    wrong commit

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -10,7 +10,7 @@ class FactorialTest {
 
     @Test
     void testFactorialOfFive() {
-        assertEquals(120, Factorial.calculate(5));
+        assertEquals(121, Factorial.calculate(5));
     }
 
     @Test
